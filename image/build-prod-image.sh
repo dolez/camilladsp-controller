86410f673f06af049a81ae5fd6d028477e312186
Vincent Dolez <vincent.dolez@gmail.com>
2025-01-21 23:31:07 +0100

    cleanup

--- image/build-prod-image.sh
+++
@@ -1,9 +0,0 @@
-docker run --platform=linux/amd64 --privileged \
-  -v $(pwd)/output:/output \
-  -v camilladsp-build:/build \
-  -v $(pwd)/create-prod-image.sh:/workdir/create-prod-image.sh \
-  -v $(pwd)/create-base-image.sh:/workdir/create-base-image.sh \
-  -v $(pwd)/overlays:/workdir/overlays \
-  -e WIFI_SSID="ORBI-78" \
-  -e WIFI_PSK="chummyzoo238" \
-  rpi-image-builder /workdir/create-prod-image.sh
--- image/create-prod-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./create-base-image.sh
-
-# Création image dev
-IMAGE_NAME="camilla-prod.img"
-main "standard"
